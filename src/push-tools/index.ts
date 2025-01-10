dd87484a4e3630720d2968241c3789b85732ab5c
Pyx <git@notpyx.me>
2025-01-10 18:21:43 -0500

    push template workflow code

--- src/push-tools/index.ts
+++ src/push-tools/index.ts
@@ -1,2 +1,30 @@
 // pyxfluff 2025
 
+import { execSync } from "child_process";
+import { existsSync, lstatSync } from "fs";
+import { resolve } from "path";
+
+function getModifications(
+    basePath: string
+): string[] {
+    try {
+        const modifiedFolders = new Set<string>();
+
+        execSync("git diff --name-only HEAD~1 HEAD", { encoding: "utf-8" }).split("\n").filter(Boolean).forEach((file) => {
+            if (resolve(file).startsWith(resolve(basePath))) {
+                const folderPath = resolve(file).split("/").slice(0, -1).join("/");
+
+                if (!modifiedFolders.has(folderPath) && existsSync(folderPath) && lstatSync(folderPath).isDirectory()) {
+                    modifiedFolders.add(folderPath);
+                }
+            }
+        });
+
+        return Array.from(modifiedFolders);
+    } catch (error) {
+        console.error("Failed:", error);
+        process.exit(1);
+    }
+}
+
+console.log(getModifications("../apps"));
